742d1471944d00d8a67e53322ff4032e4750225b
sanuatmasai <sanuatmasai@gmail.com>
2022-11-10 09:34:37 +0530

    day-1 task done

---
+++ src/com/masai/utility/DBUtil.java
@@ -0,0 +1,26 @@
+package com.masai.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	public static Connection provideConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String url = "jdbc:mysql://localhost:3306/db3";
+		
+		try {
+			conn = DriverManager.getConnection(url, "root", "12345");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
